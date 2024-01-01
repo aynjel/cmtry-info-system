d3978f56eeb573b35aa3783d35d55cebc804cbb2
anggi <ortegacanillo76@gmail.com>
2024-01-01 14:47:23 +0800

    Update cemeterymappingdb.sql and config.php

--- cemeterymappingdb.sql
+++ cemeterymappingdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 09, 2023 at 04:57 PM
+-- Generation Time: Jan 01, 2024 at 07:46 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -41,7 +41,7 @@ CREATE TABLE `tblautonumber` (
 --
 
 INSERT INTO `tblautonumber` (`ID`, `AUTOSTART`, `AUTOINC`, `AUTOEND`, `AUTOKEY`, `AUTONUM`) VALUES
-(1, '2018', 1, 13, 'PEOPLEID', 0);
+(1, '2018', 1, 19, 'PEOPLEID', 0);
 
 -- --------------------------------------------------------
 
@@ -61,8 +61,8 @@ CREATE TABLE `tblcategory` (
 
 INSERT INTO `tblcategory` (`CATEGID`, `CATEGORIES`, `USERID`) VALUES
 (10, '1', 0),
-(12, '2', 0),
-(13, '3', 0);
+(13, '3', 0),
+(16, '2', 0);
 
 -- --------------------------------------------------------
 
@@ -87,9 +87,10 @@ CREATE TABLE `tblpeople` (
 INSERT INTO `tblpeople` (`PEOPLEID`, `FNAME`, `GRAVENO`, `BORNDATE`, `DIEDDATE`, `CATEGORIES`, `LOCATION`) VALUES
 (9893, 'John Doe', 2, '2015-12-03', '2020-12-17', '1', 'Ibo'),
 (9894, 'Jane Doe', 7, '2014-12-11', '2020-12-31', '1', 'Carmen'),
-(9895, 'Kylynn Campbell', 51, '1970-09-04', '2006-01-18', '2', 'Carmen'),
+(9895, 'Kylynn Campbell', 51, '1970-09-04', '2006-01-18', '1', 'Carmen'),
 (9896, 'Colt Weiss', 84, '2009-04-05', '2013-08-07', '1', 'Luray'),
-(9897, 'Daphne Parsons', 325, '1976-09-08', '2002-11-23', '2', 'Canlumampao');
+(9897, 'Daphne Parsons', 268, '1976-09-08', '2002-11-23', '3', 'Canlumampao'),
+(201813, 'Cally Joseph', 300, '2020-11-15', '2017-05-29', '3', 'Canlumampao');
 
 -- --------------------------------------------------------
 
@@ -100,8 +101,20 @@ INSERT INTO `tblpeople` (`PEOPLEID`, `FNAME`, `GRAVENO`, `BORNDATE`, `DIEDDATE`,
 CREATE TABLE `tblreport` (
   `id` int(11) NOT NULL,
   `issue` text NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `report_type` varchar(100) NOT NULL,
+  `status` varchar(100) NOT NULL DEFAULT 'Pending',
   `created_at` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tblreport`
+--
+
+INSERT INTO `tblreport` (`id`, `issue`, `user_id`, `report_type`, `status`, `created_at`) VALUES
+(4, 'TEST', 129, 'Fraud', 'Declined', '2023-12-30 23:41:40'),
+(5, 'Nihil earum et offic', 129, 'Management', 'Pending', '2023-12-31 12:11:56'),
+(6, 'Culpa ea numquam nes', 129, 'Harrassment', 'Pending', '2024-01-01 09:56:07');
 
 -- --------------------------------------------------------
 
@@ -111,6 +124,7 @@ CREATE TABLE `tblreport` (
 
 CREATE TABLE `tblreserve` (
   `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
   `graveno` int(11) NOT NULL,
   `block` int(11) NOT NULL,
   `location` varchar(100) NOT NULL,
@@ -124,12 +138,13 @@ CREATE TABLE `tblreserve` (
 -- Dumping data for table `tblreserve`
 --
 
-INSERT INTO `tblreserve` (`id`, `graveno`, `block`, `location`, `status`, `email`, `mobile_number`, `created_at`) VALUES
-(1, 58, 1, 'Sangi', 'Contacted', 'samepl2@gmail.com', '789564321', '2023-12-09 17:04:49'),
-(2, 51, 2, 'Carmen', 'Approved', 'sample@gmail.com', '123456789', '2023-12-09 17:05:05'),
-(3, 37, 1, 'Dumlog', 'Approved', 'juzoqa@mailinator.com', '184', '2023-12-09 20:40:23'),
-(4, 84, 0, 'Luray', 'Approved', 'lelahyfe@mailinator.com', '193', '2023-12-09 20:40:31'),
-(5, 325, 2, 'Canlumampao', 'Approved', 'tusiqy@mailinator.com', '28512345', '2023-12-09 20:45:41');
+INSERT INTO `tblreserve` (`id`, `user_id`, `graveno`, `block`, `location`, `status`, `email`, `mobile_number`, `created_at`) VALUES
+(1, 129, 7, 1, 'Sangi', 'Approved', 'samepl2@gmail.com', '789564321', '2023-12-09 17:04:49'),
+(2, 129, 51, 1, 'Carmen', 'Approved', 'sample@gmail.com', '123456789', '2023-12-09 17:05:05'),
+(3, 129, 2, 1, 'Dumlog', 'Approved', 'juzoqa@mailinator.com', '184', '2023-12-09 20:40:23'),
+(4, 129, 84, 1, 'Luray', 'Declined', 'lelahyfe@mailinator.com', '193', '2023-12-09 20:40:31'),
+(9, 129, 300, 3, 'Canlumampao', 'Pending', 'mocepak@mailinator.com', '920', '2024-01-01 09:57:39'),
+(10, 129, 268, 3, 'Sangi', 'Pending', 'bova@mailinator.com', '763', '2024-01-01 09:58:29');
 
 -- --------------------------------------------------------
 
@@ -220,31 +235,31 @@ ALTER TABLE `tblautonumber`
 -- AUTO_INCREMENT for table `tblcategory`
 --
 ALTER TABLE `tblcategory`
-  MODIFY `CATEGID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `CATEGID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT for table `tblpeople`
 --
 ALTER TABLE `tblpeople`
-  MODIFY `PEOPLEID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9898;
+  MODIFY `PEOPLEID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=201819;
 
 --
 -- AUTO_INCREMENT for table `tblreport`
 --
 ALTER TABLE `tblreport`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `tblreserve`
 --
 ALTER TABLE `tblreserve`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `tbluseraccount`
 --
 ALTER TABLE `tbluseraccount`
-  MODIFY `USERID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=137;
+  MODIFY `USERID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=140;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
