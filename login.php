ccc6104bf7150dc3ca1d202060fc7a7387fca228
anggi <ortegacanillo76@gmail.com>
2024-12-07 15:01:40 +0800

    update login

--- login.php
+++ login.php
@@ -18,18 +18,8 @@
         <label class="control-label" for="U_PASS">Password</label>
         <input class="form-control" id="U_PASS" name="U_PASS" type="password" required />
       </div>
-      <!-- <div class="auth-form-group">
-        <label class="control-label" for="U_ROLE">Role</label>
-        <select class="form-control" id="U_ROLE" name="U_ROLE" required>
-          <option selected value="User">User</option>
-          <option value="Staff">Staff</option>
-        </select>
-      </div> -->
       <div class="auth-form-group">
         <button class="btn-submit" name="btnLogin" type="submit">Login</button>
-        <a class="btn-admin" href="<?php echo web_root; ?>admin/">
-          Login as Administrator
-        </a>
       </div>
     </form>
   </div>
@@ -42,26 +32,13 @@ if (isset($_POST['btnLogin'])) {
   $upass  = trim($_POST['U_PASS']);
   $h_upass = sha1($upass);
 
-  // find user in database and get user role
-  $sql = "SELECT * FROM `tbluseraccount` WHERE `U_USERNAME` = '" . $email . "'";
-  $mydb->setQuery($sql);
-  $cur = $mydb->executeQuery();
-
-  $row_count = $mydb->num_rows($cur);
-  if ($row_count == 1) {
-    $user_found = $mydb->loadSingleResult();
-    $urole = $user_found->U_ROLE;
-  } else {
-    $urole = '';
-  }
-
-  if ($email == '' or $upass == '' or $urole == '') {
+  if ($email == '' or $upass == '') {
     message("Invalid Username and Password!", "error");
   } else {
     $user = new User();
-    $res = $user::userAuthenticationWithRole($email, $h_upass, $urole);
+    $res = $user::userAuthentication($email, $h_upass);
     if ($res == true) {
-      // message("You logged in as ".$_SESSION['U_ROLE'].".","success");
+      message("You logged in as " . $_SESSION['U_ROLE'] . ".", "success");
       if ($_SESSION['U_ROLE'] == 'Staff') {
         redirect(web_root . 'staff/');
       } elseif ($_SESSION['U_ROLE'] == 'User') {
